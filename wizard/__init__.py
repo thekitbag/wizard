1ed5a32546c66b50f9deb363c023a952615c49ef
Mark Gray <mfgray87@gmail.com>
2018-11-01 12:48:08 +0000

    importing models into init

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -4,12 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
-
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-db.init_app(app)
+from wizard.models import User
 
 import wizard.views
 import wizard.messages
